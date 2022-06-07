c04e921aa2ff879a951553c573d4c9f3bfa11258
Ashaolu John <timiasha@gmail.com>
2022-06-07 13:25:27 -0700

    Hulu commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Results from '../components/Results'
+import styles from '../styles/Home.module.css'
+import requests from '../utils/requests'
+
+export default function Home( { results }) {
+  
+  return (
+    <div>
+      <Head>
+      <title>Hulu 2.0</title>
+    </Head>
+
+    {/* Header*/}
+
+    <Header/>
+
+     {/* Nav*/}
+     <Nav />
+
+      {/* Results*/}
+      <Results results={results}/>
+
+
+    </div>
+  
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+  .then((res) => res.json());
+
+  return{
+    props: {
+      results: request.results,
+    }
+  }
+
+}
