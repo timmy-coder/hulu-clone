c16f03410cd678f5222a9c8d01447cf5955da529
Ashaolu John <timiasha@gmail.com>
2022-10-10 07:20:17 -0700

    index changes

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
 import Results from '../components/Results'
-import styles from '../styles/Home.module.css'
 import requests from '../utils/requests'
 
 export default function Home( { results }) {
